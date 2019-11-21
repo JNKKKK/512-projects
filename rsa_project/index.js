a4b48336a39bfbb46497a6830d6264c78c632b46
Enkai Ji <jek.0506@outlook.com>
2019-11-21 16:08:17 -0500

    fix gitignore

--- rsa_project/index.js
+++ rsa_project/index.js
@@ -1,3 +1,10 @@
-import a from "./rsa"
+var bigInt = require("big-integer");
 
-console.log(a)
+
+import RSA from "./rsa"
+
+let r = new RSA()
+r.generate(20)
+
+console.log(r.n, r.e, r.d)
+console.log(r.encrypt('abc'))
--- rsa_project/rsa.js
+++ rsa_project/rsa.js
@@ -1,3 +1,60 @@
-a={}
+var bigInt = require("big-integer");
 
-export default a
+class RSA {
+    constructor() {
+
+    }
+
+    generate(keysize) {
+        // Generates a random k-bit prime greater than √2 × 2^(k-1)       
+        // returns a random generated prime
+        function randomPrime(bits) {
+            const min = bigInt(6074001000).shiftLeft(bits - 33);  // min ≈ √2 × 2^(bits - 1)
+            const max = bigInt.one.shiftLeft(bits).minus(1);  // max = 2^(bits) - 1
+            for (; ;) {
+                const p = bigInt.randBetween(min, max);  // WARNING: not a cryptographically secure RNG!
+                if (p.isProbablePrime(256)) {
+                    return p;
+                }
+            }
+        }
+
+        // set up variables for key generation
+        const e = bigInt(65537);  // use fixed public exponent
+        let p;
+        let q;
+        let lambda;
+
+        // generate p and q such that λ(n) = lcm(p − 1, q − 1) is coprime with e and |p-q| >= 2^(keysize/2 - 100)
+        do {
+            p = randomPrime(keysize / 2);
+            q = randomPrime(keysize / 2);
+            lambda = bigInt.lcm(p.minus(1), q.minus(1));
+        } while (bigInt.gcd(e, lambda).notEquals(1) || p.minus(q).abs().shiftRight(
+            keysize / 2 - 100).isZero());
+
+        this.n = p.multiply(q)
+        this.e = e
+        this.d = e.modInv(lambda)
+    }
+
+    encrypt(m) {
+        function str2bigInt(s) {
+            var hexString = "";
+            for (var i = 0; i < s.length; i++) {
+                hexString += ("0" + s.charCodeAt(i).toString(16)).slice(-2);
+            }
+            return new bigInt(hexString, 16);
+        }
+        return str2bigInt(m).modPow(this.e, this.n);
+    }
+
+    decrypt(c) {
+        function bigInt2str() {
+            
+        }
+        return bigInt(c).modPow(this.d, this.n);
+    };
+}
+
+export default RSA
