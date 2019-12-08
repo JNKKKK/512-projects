66a342c9938db16d14071a9a945ebfaf9ec69f6a
Enkai Ji <jek.0506@outlook.com>
2019-12-08 16:34:12 -0500

    vs

---
+++ rsa_project/animation_vs.js
@@ -0,0 +1,192 @@
+import anime from 'animejs/lib/anime.es.js'
+
+function vs_timeline(r) {
+    let tl = anime.timeline({})
+    tl
+        .add({
+            targets: '#div-5-0',
+            left: ['calc(0 - 25rem)', 'calc(50% - 12.5rem'],
+            duration: 1000,
+            easing: 'easeOutExpo',
+        })
+        .add({
+            targets: ['#div-5-1','#div-5-3'],
+            left: ['calc(0 - 50rem)', 'calc(50% - 25rem'],
+            duration: 1000,
+            easing: 'easeOutExpo',
+        })
+        .add({
+            targets: ['#div-5-2','#div-5-4'],
+            opacity: [0, 1],
+            duration: 1000,
+            easing: 'easeOutExpo',
+        })
+        //////////// RSA /////////////////////
+        // change color
+        .add({
+            targets: ['#div-5-2 .target-e','#div-5-2 .target-e'],
+            color: '#F00',
+            'border-color': '#F00',
+            duration: 1000,
+            easing: 'easeOutExpo',
+        })
+        // scale
+        .add({
+            targets: ['#div-5-2 .target-e','#div-5-2 .target-e'],
+            scale: 1.5,
+             duration: 100,
+            easing: 'easeInOutSine',
+        })
+        .add({
+            targets: ['#div-5-2 .target-e','#div-5-2 .target-e'],
+            scale: 1,
+             duration: 100,
+            easing: 'easeInOutSine',
+        })
+        .add({
+            targets: ['#div-5-2 .target-e','#div-5-2 .target-e'],
+            scale: 1.5,
+             duration: 100,
+            easing: 'easeInOutSine',
+        })
+        .add({
+            targets: ['#div-5-2 .target-e','#div-5-2 .target-e'],
+            scale: 1,
+             duration: 100,
+            easing: 'easeInOutSine',
+        })
+        // change color
+        .add({
+            targets: ['#div-5-2 .target-e','#div-5-2 .target-e'],
+            color: '#FFF',
+            'border-color': '#FFF',
+            duration: 1000,
+            easing: 'easeOutExpo',
+        })
+        .add({
+            targets: ['#div-5-2 .target-d','#div-5-2 .target-d'],
+            color: '#F00',
+            'border-color': '#F00',
+            duration: 1000,
+            easing: 'easeOutExpo',
+        })
+        // scale
+        .add({
+            targets: ['#div-5-2 .target-d','#div-5-2 .target-d'],
+            scale: 1.5,
+             duration: 100,
+            easing: 'easeInOutSine',
+        })
+        .add({
+            targets: ['#div-5-2 .target-d','#div-5-2 .target-d'],
+            scale: 1,
+             duration: 100,
+            easing: 'easeInOutSine',
+        })
+        .add({
+            targets: ['#div-5-2 .target-d','#div-5-2 .target-d'],
+            scale: 1.5,
+             duration: 100,
+            easing: 'easeInOutSine',
+        })
+        .add({
+            targets: ['#div-5-2 .target-d','#div-5-2 .target-d'],
+            scale: 1,
+             duration: 100,
+            easing: 'easeInOutSine',
+        })
+        // change color
+        .add({
+            targets: ['#div-5-2 .target-d','#div-5-2 .target-d'],
+            color: '#FFF',
+            'border-color': '#FFF',
+            duration: 1000,
+            easing: 'easeOutExpo',
+        })
+        //////////// CC /////////////////////
+         // change color
+         .add({
+            targets: ['#div-5-4 .key','#div-5-4 .target-e'],
+            color: '#F00',
+            'border-color': '#F00',
+            duration: 1000,
+            easing: 'easeOutExpo',
+        })
+        // scale
+        .add({
+            targets: ['#div-5-4 .key','#div-5-4 .target-e'],
+            scale: 1.5,
+             duration: 100,
+            easing: 'easeInOutSine',
+        })
+        .add({
+            targets: ['#div-5-4 .key','#div-5-4 .target-e'],
+            scale: 1,
+             duration: 100,
+            easing: 'easeInOutSine',
+        })
+        .add({
+            targets: ['#div-5-4 .key','#div-5-4 .target-e'],
+            scale: 1.5,
+             duration: 100,
+            easing: 'easeInOutSine',
+        })
+        .add({
+            targets: ['#div-5-4 .key','#div-5-4 .target-e'],
+            scale: 1,
+             duration: 100,
+            easing: 'easeInOutSine',
+        })
+        // change color
+        .add({
+            targets: ['#div-5-4 .key','#div-5-4 .target-e'],
+            color: '#FFF',
+            'border-color': '#FFF',
+            duration: 1000,
+            easing: 'easeOutExpo',
+        })
+        .add({
+            targets: ['#div-5-4 .key','#div-5-4 .target-d'],
+            color: '#F00',
+            'border-color': '#F00',
+            duration: 1000,
+            easing: 'easeOutExpo',
+        })
+        // scale
+        .add({
+            targets: ['#div-5-4 .key','#div-5-4 .target-d'],
+            scale: 1.5,
+             duration: 100,
+            easing: 'easeInOutSine',
+        })
+        .add({
+            targets: ['#div-5-4 .key','#div-5-4 .target-d'],
+            scale: 1,
+             duration: 100,
+            easing: 'easeInOutSine',
+        })
+        .add({
+            targets: ['#div-5-4 .key','#div-5-4 .target-d'],
+            scale: 1.5,
+             duration: 100,
+            easing: 'easeInOutSine',
+        })
+        .add({
+            targets: ['#div-5-4 .key','#div-5-4 .target-d'],
+            scale: 1,
+             duration: 100,
+            easing: 'easeInOutSine',
+        })
+        // change color
+        .add({
+            targets: ['#div-5-4 .key','#div-5-4 .target-d'],
+            color: '#FFF',
+            'border-color': '#FFF',
+            duration: 1000,
+            easing: 'easeOutExpo',
+        })
+    tl.pause()
+    return tl
+}
+
+export { vs_timeline }
--- rsa_project/index.js
+++ rsa_project/index.js
@@ -4,12 +4,13 @@ import { keygen_timeline } from './animation_keygen'
 import { keyex_timeline } from './animation_keyex'
 import { encryption_timeline_0, encryption_timeline_1 } from './animation_encrypt'
 import { decryption_timeline_0, decryption_timeline_1 } from './animation_decrypt'
+import { vs_timeline } from './animation_vs'
 
 
 let r = new RSA()
 r.generate(200)
 
-var keygen_tl, keyex_tl, encrypt_tl_0, encrypt_tl_1,decrypt_tl_0,decrypt_tl_1;
+var keygen_tl, keyex_tl, encrypt_tl_0, encrypt_tl_1, decrypt_tl_0, decrypt_tl_1, vs_tl;
 var menu_items
 
 function enter_keygen() {
@@ -76,6 +77,22 @@ function enter_decryption() {
     });
 }
 
+function enter_vs() {
+    anime({
+        targets: '#menu .item',
+        translateX: 270,
+        opacity: [1, 0],
+        easing: 'easeOutExpo',
+        duration: 500,
+        delay: anime.stagger(100),
+        complete: () => {
+            document.getElementById("menu").style.display = "none";
+            document.getElementById("vs-container").style.display = "block";
+            vs_tl.play()
+        }
+    });
+}
+
 
 window.onload = function () {
     // hide containers
@@ -89,18 +106,20 @@ window.onload = function () {
     menu_items[1].addEventListener('click', enter_keyex);
     menu_items[2].addEventListener('click', enter_encryption);
     menu_items[3].addEventListener('click', enter_decryption);
+    menu_items[4].addEventListener('click', enter_vs);
     //bind button click event
-    document.getElementById("encrypt-button").addEventListener('click', function() {
+    document.getElementById("encrypt-button").addEventListener('click', function () {
         encrypt_tl_1.play()
     });
-    document.getElementById("decrypt-button").addEventListener('click', function() {
+    document.getElementById("decrypt-button").addEventListener('click', function () {
         decrypt_tl_1.play()
     });
-    document.getElementById("back-link").addEventListener('click', function() {
+    document.getElementById("back-link").addEventListener('click', function () {
         document.getElementById("keygen-container").style.display = "none";
         document.getElementById("keyex-container").style.display = "none";
         document.getElementById("encryption-container").style.display = "none";
         document.getElementById("decryption-container").style.display = "none";
+        document.getElementById("vs-container").style.display = "none";
         document.getElementById("menu").style.display = "flex";
         anime({
             targets: '#menu .item',
@@ -119,4 +138,5 @@ window.onload = function () {
     encrypt_tl_1 = encryption_timeline_1(r)
     decrypt_tl_0 = decryption_timeline_0(r)
     decrypt_tl_1 = decryption_timeline_1(r)
+    vs_tl = vs_timeline(r)
 };
